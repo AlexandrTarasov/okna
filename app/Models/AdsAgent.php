1decb8652962521b7780b769c1381e79161aa3b6
ATarasov <usproinfo@gmail.com>
2020-10-08 23:43:41 +0300

    dear agent show all leads

--- app/Models/AdsAgent.php
+++ app/Models/AdsAgent.php
@@ -4,10 +4,10 @@ namespace AppM;
 
 class AdsAgent extends Model
 {
-	public function getAdsAgentEnquiries()
+	public function getAdsAgentEnquiries($source = '')
 	{
 		$sql ="select leads.*, c.name as client_name from `leads` left join clients as c on (leads.client_id = c.id)
-			where source = 'dear-agent' order by id desc limit 30 ";
+			 order by id desc limit 130 ";
 		return $this->db->row($sql);	
 	}
 }
